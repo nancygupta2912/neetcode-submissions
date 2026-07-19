91ead552c74d338d6604e3e5f2c1a1effa7f6f21
Nancy Gupta <155845911+nancygupta2912@users.noreply.github.com>
2026-07-19 15:29:09 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int resLen=0;
+        int resIdx=0;
+
+        for(int i=0;i<s.size();i++){
+            //odd
+            int l=i;
+            int r=i;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>resLen){
+                    resLen=r-l+1;
+                    resIdx=l;
+                }
+                l--;
+                r++;
+            }
+
+            //even
+            l=i;
+            r=i+1;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>resLen){
+                    resLen=r-l+1;
+                    resIdx=l;
+                }
+                l--;
+                r++;
+            }
+
+        }
+        return s.substr(resIdx,resLen);
+    }
+};
