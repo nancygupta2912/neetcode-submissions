fe774f53929461948a35f266f5cfefbcf0290d44
Nancy Gupta <155845911+nancygupta2912@users.noreply.github.com>
2026-07-19 15:36:41 +0530

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int lcs(string text1, string text2,int m,int n,vector<vector<int>>&dp){
+    if(m<0 || n<0) return 0;
+    if(dp[m][n]!=-1) return dp[m][n];
+
+    if(text1[m]==text2[n]){
+        return 1+lcs(text1,text2,m-1,n-1,dp);
+    }
+    return dp[m][n]= max(lcs(text1,text2,m,n-1,dp),lcs(text1,text2,m-1,n,dp));
+
+}
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size();
+        int n=text2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return lcs(text1,text2,m-1,n-1,dp);
+    }
+};
